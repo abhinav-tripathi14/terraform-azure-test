c8eb9f4f7b536e90b91e754a74e9bc2202299b91
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-05-25 20:24:48 +0530

    Update main.tf

--- tests/setup/main.tf
+++ tests/setup/main.tf
@@ -14,20 +14,4 @@ resource "random_pet" "rg_name_prefix" {
 output "rg_name_prefix" {
   value = random_pet.rg_name_prefix.id
 }
-provider "azurerm" {
-  features {}
-}
 
-
-variable "rg" {
-    type = string
-    default = "exammple-rg-abhinav"
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = var.rg
-  location = "West Europe"
-}
-output "rg-name" {
-  value = azurerm_resource_group.example.name
-}
